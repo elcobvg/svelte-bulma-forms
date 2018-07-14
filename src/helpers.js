3f3e249fc58c9252a15a5608b5250f67659ec2a3
Elco Brouwer von Gonzenbach <elco.brouwer@gmail.com>
2018-07-14 09:43:41 +0700

    Cleanup, moved helper functions to... helpers

---
+++ src/helpers.js
@@ -0,0 +1,7 @@
+import uuid from 'random-uuid-v4'
+
+export const id = name => name ? `${name.toLowerCase()}-${uuid()}` : uuid()
+
+export const stateClass = state => state ? ` is-${state}` : ''
+
+export const sizeClass = size => size ? ` is-${size}` : ''
